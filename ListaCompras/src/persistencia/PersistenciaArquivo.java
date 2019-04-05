20f8ea57cf5d779c5375cdab184c5c1be89fab10
20162bsi0236 <20162bsi0236@SR6709.cefetes.br>
2019-04-05 11:01:35 -0300

    Organização dos package

--- ListaCompras/src/persistencia/PersistenciaArquivo.java
+++ ListaCompras/src/persistencia/PersistenciaArquivo.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package arquivo;
+package persistencia;
 
 import java.io.FileWriter;
 import java.io.IOException;
