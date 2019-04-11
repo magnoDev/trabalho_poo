67619f500adc237262a13a7c4a1f6191f6c62799
David de Moura Marques <mouradavid8@gmail.com>
2019-04-11 09:28:48 -0300

    Produto

--- ListaCompras/src/dominio/produto/Produto.java
+++ ListaCompras/src/dominio/produto/Produto.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package dominio;
+package dominio.produto;
 
 import java.io.Serializable;
 
@@ -11,6 +11,8 @@ import java.io.Serializable;
  *
  * @author david
  */
-public class Produto implements Serializable{
-    
+public class Produto extends ProdutoBase implements Serializable{
+    private double valor;
+    private String dataValidade;
+    private String lote;
 }
---
+++ ListaCompras/src/dominio/produto/ProdutoBase.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dominio.produto;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author david
+ */
+public class ProdutoBase implements Serializable{
+    private String nome;
+    private String categoria;
+    private String tipo;
+    private double peso;
+    private String marca;
+    
+    
+    
+}
