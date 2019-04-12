e5301ac086f6c0ba148511114a101520397bfe84
David de Moura Marques <mouradavid8@gmail.com>
2019-04-12 09:50:52 -0300

    Produto

--- ListaCompras/src/dominio/produto/Produto.java
+++ ListaCompras/src/dominio/produto/Produto.java
@@ -15,4 +15,53 @@ public class Produto extends ProdutoBase implements Serializable{
     private double valor;
     private String dataValidade;
     private String lote;
+
+    public Produto(double valor, String dataValidade, String lote, String nome, 
+            String categoria, String tipo, double peso, String marca) {
+        super(nome, categoria, tipo, peso, marca);
+        this.valor = valor;
+        this.dataValidade = dataValidade;
+        this.lote = lote;
+    }
+    
+    public Produto(String valor, String dataValidade, String lote, String nome, 
+            String categoria, String tipo, String peso, String marca) {
+        super(nome, categoria, tipo, peso, marca);
+        this.valor = Double.parseDouble(valor);
+        this.dataValidade = dataValidade;
+        this.lote = lote;
+    }
+    
+    public String toString(Produto produto){
+        return produto.toString() + "," 
+                + produto.valor + ","
+                + produto.dataValidade + ","
+                + produto.lote;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public String getDataValidade() {
+        return dataValidade;
+    }
+
+    public String getLote() {
+        return lote;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public void setDataValidade(String dataValidade) {
+        this.dataValidade = dataValidade;
+    }
+
+    public void setLote(String lote) {
+        this.lote = lote;
+    }
+    
+    
 }
